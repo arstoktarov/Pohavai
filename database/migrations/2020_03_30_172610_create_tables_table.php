f76d925a67fc739b5cbd050a96cb31b96ccdf022
Arsen Toktarov <back@1-000.kz>
2020-03-30 23:42:56 +0600

    Added users, restaurants, table models

---
+++ database/migrations/2020_03_30_172610_create_tables_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('number');
+            $table->unsignedBigInteger('restaurant_id');
+            $table->integer('seats');
+            $table->enum('status', ['reserved', 'occupied', 'vacant'])->default('vacant');
+            $table->timestamp('reserved_at')->nullable();
+            $table->timestamps();
+            $table->foreign('restaurant_id')->references('id')->on('restaurants');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tables');
+    }
+}
